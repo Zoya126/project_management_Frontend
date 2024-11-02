60b684633cb8030b5a0a41f8da0d788c50f81bba
Zoya126 <91610410+Zoya126@users.noreply.github.com>
2024-11-02 23:19:18 +0530

    ...

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,4 +1,8 @@
-const BACKEND_URL= 'https://proj-management-backend-2.onrender.com'
+const BACKEND_URL= 'https://proj-management-backend.onrender.com'
+
+// const BACKEND_URL= 'http://localhost:3003'
+
+
 
 
 export {BACKEND_URL};
