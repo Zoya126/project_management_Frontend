40622a3042e5367059be812bc97709074a13ccb2
Zoya126 <91610410+Zoya126@users.noreply.github.com>
2024-11-02 22:19:18 +0530

    url enhancement

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,4 +1,4 @@
-// const BACKEND_URL= 'https://pro-manage-9m50.onrender.com'
-const BACKEND_URL= 'http://localhost:3003';
+const BACKEND_URL= 'https://proj-management-backend-2.onrender.com'
+
 
 export {BACKEND_URL};
